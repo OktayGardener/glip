b78f4378ac4dadf0568df8496ca76c19a3d99c04
Oktay Gardener <oktayb@kth.se>
2016-11-26 20:25:56 +0100

    bye label

--- app/visufy/forms.py
+++ app/visufy/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SongForm(forms.Form):
-    song_title = forms.CharField(widget=forms.TextInput(attrs={'id' : 'song_title'}))
+    song_title = forms.CharField(label='', widget=forms.TextInput(attrs={'id' : 'song_title'}))
